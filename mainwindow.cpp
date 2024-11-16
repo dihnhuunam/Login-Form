92423b42d07787123a25ac968da5496f9d3c0fcb
dihnhuunam <dihnhuunam@gmail.com>
2024-11-17 01:36:29 +0700

    [ADD]: Login Form

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QString>
+#include <QtWidgets>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setupStyles();
+    setupLayout();
+    setupConnections();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +20,79 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::setupStyles()
+{
+    QFile styleFile(":/style.qss");
+    if (styleFile.open(QFile::ReadOnly)) {
+        QString style = QString::fromUtf8(styleFile.readAll());
+        this->setStyleSheet(style);
+        styleFile.close();
+    }
+
+    ui->loginLabel->setText(QString::fromUtf8("Đăng Nhập"));
+    ui->loginLabel->setAlignment(Qt::AlignCenter);
+
+    ui->usernameLineEdit->setPlaceholderText("Tài Khoản");
+    ui->usernameLineEdit->setAlignment(Qt::AlignLeft);
+    ui->usernameLineEdit->setFocus();
+
+    ui->passwordLineEdit->setPlaceholderText("Mật Khẩu");
+    ui->passwordLineEdit->setAlignment(Qt::AlignLeft);
+    ui->passwordLineEdit->setEchoMode(QLineEdit::Password);
+
+    ui->loginButton->setText(QString::fromUtf8("Đăng Nhập"));
+
+    ui->statusLabel->setVisible(false);
+    ui->statusLabel->setAlignment(Qt::AlignCenter);
+}
+
+void MainWindow::setupLayout()
+{
+    QVBoxLayout *mainLayout = new QVBoxLayout();
+
+    QHBoxLayout *hLayout = new QHBoxLayout();
+    QVBoxLayout *vLayout = new QVBoxLayout();
+
+    vLayout->addWidget(ui->loginLabel);
+    vLayout->addWidget(ui->usernameLineEdit);
+    vLayout->addWidget(ui->passwordLineEdit);
+    vLayout->addWidget(ui->loginButton);
+    vLayout->addWidget(ui->statusLabel);
+
+    hLayout->addSpacerItem(new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum));
+    hLayout->addLayout(vLayout);
+    hLayout->addSpacerItem(new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum));
+
+    mainLayout->addSpacerItem(new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding));
+    mainLayout->addLayout(hLayout);
+    mainLayout->addSpacerItem(new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding));
+
+    ui->centralwidget->setLayout(mainLayout);
+}
+
+void MainWindow::setupConnections()
+{
+    connect(ui->usernameLineEdit, &QLineEdit::returnPressed, this, &MainWindow::on_loginButton_clicked);
+    connect(ui->passwordLineEdit, &QLineEdit::returnPressed, this, &MainWindow::on_loginButton_clicked);
+}
+
+void MainWindow::on_loginButton_clicked()
+{
+    QString username = ui->usernameLineEdit->text();
+    QString password = ui->passwordLineEdit->text();
+    QString successColor = "#6BBF59";
+    QString errorColor = "#FF6B6B";
+
+    if(username == "test" && password == "test")
+    {
+        ui->statusLabel->setVisible(true);
+        ui->statusLabel->setStyleSheet(QString("color: %1;").arg(successColor));
+        ui->statusLabel->setText("Đăng nhập thành công.");
+    }
+    else
+    {
+        ui->statusLabel->setVisible(true);
+        ui->statusLabel->setStyleSheet(QString("color: %1;").arg(errorColor));
+        ui->statusLabel->setText("Đăng nhập thất bại.");
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_loginButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    void setupStyles();
+    void setupLayout();
+    void setupConnections();
 };
 #endif // MAINWINDOW_H
