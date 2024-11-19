43a8ce537844b8767639d2b05585bca51b5ec58b
dihnhuunam <dihnhuunam@gmail.com>
2024-11-19 15:45:11 +0700

    [ADD]: Animated Initialize

---
+++ Includes/AnimatedLineEdit.h
@@ -0,0 +1,14 @@
+#ifndef ANIMATED_LINE_EDIT_H
+#define ANIMATED_LINE_EDIT_H
+
+#include <QPropertyAnimation>
+#include <QWidget>
+#include <QLineEdit>
+#include <QLabel>
+
+class AnimatedLineEdit : public QWidget
+{
+    Q_OBJECT
+};
+
+#endif /* TOOLTIPS_H */
---
+++ Sources/AnimatedLineEdit.cpp
@@ -0,0 +1,1 @@
+#include "AnimatedLineEdit.h"
